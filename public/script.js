8668d5ee0006c5e9f7e8bc725bf6b39de57d74e1
kilianr20 <60264698+kilianr20@users.noreply.github.com>
2021-12-03 16:12:19 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ var browser = browser || chrome
 const server_endpoint = 'https://youtube-converter-jzqdy.ondigitalocean.app/'
 const download_endpoint = 'https://youtube-converter-jzqdy.ondigitalocean.app/download/'
 const get_info_endpoint = 'https://youtube-converter-jzqdy.ondigitalocean.app/getInfo/'
-const wakeup_dlp_endpoint = 'https://yt-dlp-back.herokuapp.com/wakeup'
+const wakeup_dlp_endpoint = 'https://yt-dlp-back-pmymh.ondigitalocean.app/wakeup'
 
 // locally
 // const server_endpoint = 'http://localhost:3000/'
